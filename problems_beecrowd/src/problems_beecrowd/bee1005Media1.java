c65cfa68f297929bcb39275fba7a6a54da96ad38
developerbreno081 <developerbreno081@gmail.com>
2024-10-25 15:57:29 -0300

    Upload bee - 1003, 1004 e 1005

---
+++ problems_beecrowd/src/problems_beecrowd/bee1005Media1.java
@@ -0,0 +1,31 @@
+package problems_beecrowd;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class bee1005Media1 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o seu nome: ");
+		String nome = sc.next();
+		
+		System.out.print("Digite a primeira nota: ");
+		double a = sc.nextDouble();
+		
+		System.out.print("Digite a segunda nota: ");
+		double b = sc.nextDouble();
+		
+		double mediaPonderada = (a * 3.5 + b * 7.5) / 11;
+		
+		System.out.printf("%s sua média foi:%n", nome);
+		System.out.printf("MEDIA = %.5f", mediaPonderada);
+		
+		
+
+	}
+
+}
