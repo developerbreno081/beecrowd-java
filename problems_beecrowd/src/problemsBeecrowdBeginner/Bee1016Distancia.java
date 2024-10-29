fc4f35293c5473c14755a80d30f06c28f94f70d2
developerbreno081 <developerbreno081@gmail.com>
2024-10-28 22:31:28 -0300

    Upload new file

---
+++ problems_beecrowd/src/problemsBeecrowdBeginner/Bee1016Distancia.java
@@ -0,0 +1,18 @@
+package problemsBeecrowdBeginner;
+
+import java.util.Scanner;
+
+public class Bee1016Distancia {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int distancia = sc.nextInt();
+		int calculo = distancia * 2;
+		System.out.printf("%d ", calculo);
+		
+		sc.close();
+	}
+
+}
